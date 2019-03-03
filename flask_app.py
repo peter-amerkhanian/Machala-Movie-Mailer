84cc7d09f7d1916735cfbc1c20d96888ff637a58
Peter Amerkhanian <peteamer@gmail.com>
2019-03-03 17:02:47 -0500

    fixed app, added sqlite db for dev

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,49 @@
-from flask import Flask, render_template, Markup
+from flask import Flask, render_template, Markup, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-with open("mail.txt", "r") as file:
-    mail = Markup(file.readline())
+app.config['SECRET_KEY'] = os.urandom(16)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
 
 
 @app.route("/")
-def test():
-    return render_template('index.html', mail=mail)
+@app.route("/home")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.email.data}.", "success")
+        return redirect(url_for('home'))
+    return render_template('register.html',
+                           title='Register',
+                           form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"{form.email.data} has been unsubscribed", "success")
+        return redirect(url_for('home'))
+    return render_template('login.html',
+                           title='Login',
+                           form=form)
 
 
 if __name__ == "__main__":
