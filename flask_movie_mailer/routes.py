8639d61ad394298f7f213e9d29e73605a7ba4f3d
Peter Amerkhanian <peteamer@gmail.com>
2019-03-06 11:47:31 -0500

    landing pages for unsub/sub, css changes

--- flask_movie_mailer/routes.py
+++ flask_movie_mailer/routes.py
@@ -3,12 +3,17 @@ from flask_movie_mailer import app, db
 from flask_movie_mailer.forms import RegistrationForm, UnsubscribeForm
 from flask_movie_mailer.models import User
 from flask_movie_mailer.quote_generator import generate_random_quote
-
+from flask_movie_mailer.movie_today import movie_today
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('index.html')
+
+
+@app.route("/landing")
+def landing():
+    return render_template('layout.html')
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -20,8 +25,13 @@ def register():
                     location=form.location.data)
         db.session.add(user)
         db.session.commit()
-        flash("Account created for {}.".format(form.email.data), "success")
-        return redirect(url_for('home'))
+        flash(Markup(
+            """
+            <b>Account created for {} in {} <i class="em em-sunglasses"></i></b> <br/>
+            <p></p>
+            {}
+            """.format(form.email.data, form.location.data, movie_today)))
+        return redirect(url_for('landing'))
     return render_template('register.html',
                            title='Register',
                            form=form)
@@ -37,14 +47,20 @@ def unsubscribe():
         quote = generate_random_quote()
         flash(Markup(
             """
-           <b>{} has been unsubscribed... <i class="em em-cry"></i></b> <br/>
+           <b>{} has been unsubscribed... <i class="em em-cry"></i></b>
            <p></p>
-        <div class="center">
-           <i>"{}"</i> <br/>
-           <sub>{}</sub>
-        </div>
+            <div class="center">
+               <i>"{}"</i> <br/>
+               <sub>{}</sub>
+            </div>
+            <p></p>
+            <form class="form-inline d-flex justify-content-center">
+                <a class="btn btn-primary mb-4 mx-1" href="/register" role="button">
+                    Sign back up <i class="em em---1"></i>
+                </a>
+            </form>
             """.format(form.email.data, quote["text"], quote["author"])))
-        return redirect(url_for('home'))
+        return redirect(url_for('landing'))
     return render_template('unsubscribe.html',
                            title='Login',
                            form=form)
