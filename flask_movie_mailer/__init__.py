b3f6bc11e16946dcce39cef114b09a7bf9c350b4
Peter Amerkhanian <peteamer@gmail.com>
2019-03-21 13:38:10 -0500

    trouble shooting token issue

--- flask_movie_mailer/__init__.py
+++ flask_movie_mailer/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(16)
-app.config['WTF_CSRF_SECRET_KEY'] = None
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
