6a4d4f3554e4af074d7fdde2d6f2e3759bcf3309
Peter Amerkhanian <peteamer@gmail.com>
2019-03-22 16:52:41 -0500

    put db address in an enviro var

--- flask_movie_mailer/__init__.py
+++ flask_movie_mailer/__init__.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(16)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_ADDRESS']
 db = SQLAlchemy(app)
 
 from flask_movie_mailer import routes
