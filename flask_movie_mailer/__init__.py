93bb8478ba63acaac2be2ad6d4b1e8cd7e4a835d
Peter Amerkhanian <peteamer@gmail.com>
2019-05-01 14:21:47 -0500

    moved db address to private_variables module

--- flask_movie_mailer/__init__.py
+++ flask_movie_mailer/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-
+from machala_movie_mailer.private_variables import db_address
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(16)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_ADDRESS']
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_ADDRESS']
+app.config['SQLALCHEMY_DATABASE_URI'] = db_address
 db = SQLAlchemy(app)
 
 from flask_movie_mailer import routes
